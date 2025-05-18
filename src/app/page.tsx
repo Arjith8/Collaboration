30f4b8f9418820e36eb76aeb48fd1048c64f9e6c
Arjithar8 <arjithar8@gmail.com>
2025-05-18 11:07:09 +0530

    Added basic themeswitcher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { ThemeToggle } from "@/components/ui/themeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
+
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
@@ -49,6 +51,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <ThemeToggle />
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
